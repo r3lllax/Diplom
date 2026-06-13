0b972bf2a7a1ca00f5d96ec8372898e284b538c7
r3lllax <turkevicaleksandr@gmail.com>
2026-06-14 00:11:33 +0300

    All playlists songs count fix

--- repositories/repository/playlistRepository.go
+++ repositories/repository/playlistRepository.go
@@ -177,16 +177,16 @@ func (r *PlaylistRepository) GetPlaylists(ctx context.Context, userID, from, cou
   p.volume_path,
   p.is_private,
   p.is_available,
-  count(ps.id) as "songs_count",
-  coalesce(sum(s.duration),0) as "playlist_duration",
+  count(CASE WHEN s.is_available = true OR s.user_id = $1 THEN ps.id END) as "songs_count",
+  coalesce(sum(CASE WHEN s.is_available = true OR s.user_id = $1 THEN s.duration ELSE 0 END), 0) as "playlist_duration",
   (select count(1) from liked_playlists lp where lp.playlist_id = p.id and lp.user_id <> p.user_id) as "likes_count",
-    (exists(select 1 from liked_playlists lp where lp.playlist_id = p.id and lp.user_id = $1)) as "is_liked"
+  (exists(select 1 from liked_playlists lp where lp.playlist_id = p.id and lp.user_id = $1)) as "is_liked"
 from playlists p
 join users a on p.user_id = a.id
 left join playlists_songs ps on ps.playlist_id = p.id
 left join songs s on s.id = ps.song_id
-where ((p.is_private = false and p.is_available = true ) or p.user_id = $1)
-group by p.id,p.user_id,a.name,a.photo_file,p.title,p.description,p.volume_path,p.is_private,p.is_available
+where ((p.is_private = false and p.is_available = true) or p.user_id = $1)
+group by p.id, p.user_id, a.name, a.photo_file, p.title, p.description, p.volume_path, p.is_private, p.is_available
 offset $2
 limit $3
 `
