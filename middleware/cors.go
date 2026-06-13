cd2925d9aa9583acb00d4804fc767d20a8cfbe90
root <root@80-93-61-64.swtest.ru>
2026-06-13 20:36:17 +0300

    VPS Fixes:cors,refresh cookie

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,7 +6,7 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://80.93.61.64")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
